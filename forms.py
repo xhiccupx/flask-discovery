69a73cba39633a2e127ac93945d24d7beb232983
xhiccupx <smdar7@gmail.com>
2021-07-01 17:33:46 +0100

    final

--- forms.py
+++ forms.py
@@ -1,19 +1,19 @@
-from flask_wtf import Form
+from flask_wtf import Form 
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(Form):
-    first_name = StringField('First name',validators=[DataRequired("please enter your first name.")])
-    last_name = StringField('Last name',validators=[DataRequired("please enter your last name.")])
-    email = StringField('Email',validators=[DataRequired("please enter your email address."),Email("please enter your email address.")])
-    password = PasswordField('Password',validators=[DataRequired("please enter your password."),Length(min=6,message="Password must be 6 character om more.")])
-    submit = SubmitField('Sign up')
+  first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
+  last_name = StringField('Last name', validators=[DataRequired("Please enter your last name.")])
+  email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
+  password = PasswordField('Password', validators=[DataRequired("Please enter a password."), Length(min=6, message="Passwords must be 6 characters or more.")])
+  submit = SubmitField('Sign up')
 
 class LoginForm(Form):
-    email = StringField('Email',validators=[DataRequired("please enter your email address."),Email("please enter your email address.")])
-    password = PasswordField('Password',validators=[DataRequired("please enter your password")])
-    submit = SubmitField('Sign in')
+  email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
+  password = PasswordField('Password', validators=[DataRequired("Please enter a password.")])
+  submit = SubmitField("Sign in")
 
-class AdderssForm(Form):
-    address = StringField('Address',validators=[DataRequired("please enter your address.")])
-    submit = SubmitField('Search')
+class AddressForm(Form):
+  address = StringField('Address', validators=[DataRequired("Please enter an address.")])
+  submit = SubmitField("Search")
--- models.py
+++ models.py
@@ -1,77 +1,76 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
+import os
 import geocoder
-import urllib2
+import urllib.request as urllib2
 import json
+
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = 'users'
-    uid =db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(100))
-    lastname = db.Column(db.String(100))
-    email = db.Column(db.String(120))
-    pwdhash = db.Column(db.String(54))
+  __tablename__ = 'users'
+  uid = db.Column(db.Integer, primary_key = True)
+  firstname = db.Column(db.String(100))
+  lastname = db.Column(db.String(100))
+  email = db.Column(db.String(120), unique=True)
+  pwdhash = db.Column(db.String(54))
 
-    def __init__(self,firstname,lastname,email,password):
-        self.firstname = firstname.title()
-        self.lastname = lastname.title()
-        self.email = email.lower()
-        self.set_password(password)
+  def __init__(self, firstname, lastname, email, password):
+    self.firstname = firstname.title()
+    self.lastname = lastname.title()
+    self.email = email.lower()
+    self.set_password(password)
+     
+  def set_password(self, password):
+    self.pwdhash = generate_password_hash(password)
 
-    def set_password(self,password):
-        self.pwdhash =generate_password_hash(password)
+  def check_password(self, password):
+    return check_password_hash(self.pwdhash, password)
 
-    def check_password(self, password):
-        return check_password_hash(self.pwdhash,password) 
-
-
-# p = Places()
-# places = p.query("1600 Amphitheater parkway Mountain View CA")
+# p = Place()
+# places = p.query("1600 Amphitheater Parkway Mountain View CA")
 class Place(object):
-    def meters_to_walking_time(self, meters):
-        # 80 meters is one minite time
-        return int(meters/80)
+  def meters_to_walking_time(self, meters):
+    # 80 meters is one minute walking time
+    return int(meters / 80)  
 
-    def wiki_paths(self, slug):
-        return urllib2.urlparse.urljoin("http://en.wikipedia.org/wiki/", slug.replace(' ','_'))
+  def wiki_path(self, slug):
+    return urllib2.urlparse.urljoin("http://en.wikipedia.org/wiki/", slug.replace(' ', '_'))
+  
+  def address_to_latlng(self, address):
+    g = geocoder.google(address)
+    return (g.lat, g.lng)
 
-    def address_to_latlng(self, address):
-        g = geocoder.google(address)
-        return (g.lat, g.lng)
+  def query(self, address):
+    lat, lng = self.address_to_latlng(address)
+    
+    query_url = 'https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=5000&gscoord={0}%7C{1}&gslimit=20&format=json'.format(lat, lng)
+    g = urllib2.urlopen(query_url)
+    results = g.read()
+    g.close()
 
-    def query(self, address):
-        lat, lng = self.address_to_latlng(address)
-        print (lat, lng)
+    data = json.loads(results)
+    
+    places = []
+    for place in data['query']['geosearch']:
+      name = place['title']
+      meters = place['dist']
+      lat = place['lat']
+      lng = place['lon']
 
-        query_url = 'http://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=5000&gscore={0}%7C{1}&gslimit=20&format=json'.format(lat,lng)
-        g = urllib2.urlopen(query_url)
-        result = g.read()
-        g.close()
+      wiki_url = self.wiki_path(name)
+      walking_time = self.meters_to_walking_time(meters)
 
-        data = json.loads(results)
-        print (data)
+      d = {
+        'name': name,
+        'url': wiki_url,
+        'time': walking_time,
+        'lat': lat,
+        'lng': lng
+      }
 
-        place = []
-        for place in data['query']['geosearch']:
-            name = place['title']
-            meters = place['dist']
-            lat = place['lat']
-            lng = place['lng']
+      places.append(d)
 
-            wiki_url = self.wiki_path(name)
-            walking_time = self.meters_to_walking_time(meters)
-
-            d = {
-                'name': name,
-                'url': wiki_url,
-                'time': walking_time,
-                'lat': lat,
-                'lng': lng
-            }
-            
-            place.append(d)
-
-        return places    
+    return places
